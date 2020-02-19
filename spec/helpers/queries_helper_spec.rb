d66b88ca4cc89ccb51c34a62b202c9bc93a6f43e
Vincent Robert <vincent.robert@nanego.com>
2020-02-19 11:24:06 +0100

    Add missing fixture in specs

--- spec/helpers/queries_helper_spec.rb
+++ spec/helpers/queries_helper_spec.rb
@@ -4,6 +4,8 @@ require 'redmine_projects_queries_extension/queries_helper_patch'
 
 describe ProjectsQueriesHelper, type: :helper do
 
+  fixtures :projects
+
   it "should display parent column as a link to a project" do
     query = ProjectQuery.new(:name => '_', :column_names => ["name", "parent_id"])
     content = column_value(QueryColumn.new(:parent_id), query.results_scope.select{|e| e.parent_id == 1}.first, Project.find(1))
