93c7d85aac9753f812b7f994abaa44ae8492072a
Vincent Robert <vincent.robert@nanego.com>
2020-02-19 17:00:49 +0100

    Remove optional migrations

--- db/migrate/20171206111204_index_foreign_keys_in_projects.rb
+++
@@ -1,7 +0,0 @@
-class IndexForeignKeysInProjects < ActiveRecord::Migration[4.2]
-  def change
-    add_index :projects, :default_assigned_to_id
-    add_index :projects, :default_version_id
-    add_index :projects, :parent_id
-  end
-end
