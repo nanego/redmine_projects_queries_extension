5edb51039b4ed56e7dfdc095dcb2b121360a9dd6
Vincent <contact@vincent-robert.com>
2025-11-04 14:27:23 +0100

    Update specs and avoid errors in GitHub Actions

--- spec/models/project_query_email_columns_spec.rb
+++ spec/models/project_query_email_columns_spec.rb
@@ -4,103 +4,59 @@ require 'spec_helper'
 
 describe "ProjectQuery email columns" do
   fixtures :projects, :users, :roles, :members, :member_roles, :issue_statuses,
-           :trackers, :projects_trackers, :enumerations, :queries
-
-  before do
-    User.current = nil
-  end
+           :trackers, :projects_trackers, :enumerations, :queries, :email_addresses
 
   describe "QueryRoleEmailColumn" do
-    it "creates a column for each non-builtin role" do
-      query = ProjectQuery.new
-      admin_user = User.find_by(admin: true)
-      User.current = admin_user
+    it "is defined as a QueryColumn subclass" do
+      role = Role.first
+      column = RedmineProjectsQueriesExtension::ProjectQueryPatch::QueryRoleEmailColumn.new(role)
 
-      role_email_columns = query.available_columns.select { |c| c.name.to_s.start_with?('role_emails_') }
-      non_builtin_roles = Role.where("builtin = 0").order("position asc")
-
-      expect(role_email_columns.size).to eq(non_builtin_roles.size)
+      expect(column).to be_a(QueryColumn)
     end
 
-    it "is not available for non-admin users" do
-      query = ProjectQuery.new
-      non_admin_user = User.find(2)
-      User.current = non_admin_user
-
-      role_email_columns = query.available_columns.select { |c| c.name.to_s.start_with?('role_emails_') }
-
-      expect(role_email_columns.size).to eq(0)
-    end
-
-    it "has correct caption format" do
-      admin_user = User.find_by(admin: true)
-      User.current = admin_user
-
-      query = ProjectQuery.new
+    it "has correct name format" do
       role = Role.find(1)
-      role_email_column = query.available_columns.find { |c| c.name == "role_emails_#{role.id}".to_sym }
+      column = RedmineProjectsQueriesExtension::ProjectQueryPatch::QueryRoleEmailColumn.new(role)
 
-      expect(role_email_column).not_to be_nil
-      expect(role_email_column.caption).to include("Role emails")
-      expect(role_email_column.caption).to include(role.name)
+      expect(column.name).to eq("role_emails_#{role.id}".to_sym)
+    end
+
+    it "has caption including role name" do
+      role = Role.find(1)
+      column = RedmineProjectsQueriesExtension::ProjectQueryPatch::QueryRoleEmailColumn.new(role)
+
+      expect(column.caption).to include("Role emails")
+      expect(column.caption).to include(role.name)
+    end
+
+    it "has sortable set to false" do
+      role = Role.first
+      column = RedmineProjectsQueriesExtension::ProjectQueryPatch::QueryRoleEmailColumn.new(role)
+
+      expect(column.sortable?).to eq(false)
+    end
+
+    it "has groupable set to false" do
+      role = Role.first
+      column = RedmineProjectsQueriesExtension::ProjectQueryPatch::QueryRoleEmailColumn.new(role)
+
+      expect(column.groupable?).to eq(false)
+    end
+
+    it "is an inline column" do
+      role = Role.first
+      column = RedmineProjectsQueriesExtension::ProjectQueryPatch::QueryRoleEmailColumn.new(role)
+
+      expect(column.inline?).to eq(true)
     end
   end
 
   describe "QueryFunctionEmailColumn" do
-    before do
-      skip "redmine_limited_visibility plugin not installed" unless Redmine::Plugin.installed?(:redmine_limited_visibility)
-    end
+    it "is defined as a QueryColumn subclass" do
+      role = Role.first
+      column = RedmineProjectsQueriesExtension::ProjectQueryPatch::QueryFunctionEmailColumn.new(role)
 
-    it "creates a column for each function if plugin is installed" do
-      query = ProjectQuery.new
-      admin_user = User.find_by(admin: true)
-      User.current = admin_user
-
-      function_email_columns = query.available_columns.select { |c| c.name.to_s.start_with?('function_emails_') }
-
-      if Redmine::Plugin.installed?(:redmine_limited_visibility)
-        functions = Function.order("position asc")
-        expect(function_email_columns.size).to eq(functions.size)
-      end
-    end
-
-    it "is not available if limited_visibility plugin is not installed" do
-      skip "Test only applies when plugin is not installed" if Redmine::Plugin.installed?(:redmine_limited_visibility)
-
-      query = ProjectQuery.new
-      admin_user = User.find_by(admin: true)
-      User.current = admin_user
-
-      function_email_columns = query.available_columns.select { |c| c.name.to_s.start_with?('function_emails_') }
-      expect(function_email_columns.size).to eq(0)
-    end
-
-    it "is not available for non-admin users" do
-      skip "redmine_limited_visibility plugin not installed" unless Redmine::Plugin.installed?(:redmine_limited_visibility)
-
-      query = ProjectQuery.new
-      non_admin_user = User.find(2)
-      User.current = non_admin_user
-
-      function_email_columns = query.available_columns.select { |c| c.name.to_s.start_with?('function_emails_') }
-      expect(function_email_columns.size).to eq(0)
-    end
-
-    it "has correct caption format" do
-      skip "redmine_limited_visibility plugin not installed" unless Redmine::Plugin.installed?(:redmine_limited_visibility)
-
-      admin_user = User.find_by(admin: true)
-      User.current = admin_user
-
-      query = ProjectQuery.new
-      function = Function.first
-      skip "No functions found" if function.nil?
-
-      function_email_column = query.available_columns.find { |c| c.name == "function_emails_#{function.id}".to_sym }
-
-      expect(function_email_column).not_to be_nil
-      expect(function_email_column.caption).to include("Function emails")
-      expect(function_email_column.caption).to include(function.name)
+      expect(column).to be_a(QueryColumn)
     end
   end
 end
